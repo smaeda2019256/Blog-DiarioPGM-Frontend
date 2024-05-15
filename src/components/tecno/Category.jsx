80d88b1315eb68d02b4e715f7b77eff0565a97e5
Santiago Maeda <smaeda-2019256@Kinal.edu.gt>
2024-05-15 16:26:28 -0600

    Se sube el slider para las tecnologias

---
+++ src/components/tecno/Category.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react"
+import "./category.css"
+import { category } from "../../assets/data/data"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import Slider from "react-slick"
+import { GrFormPrevious } from "react-icons/gr"
+import { MdNavigateNext } from "react-icons/md"
+import axios from "axios"
+import { useLocation } from "react-router-dom"
+import { Link } from "react-router-dom"
+
+
+export const Category = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  }
+
+  return (
+    <>
+      <section className='category'>
+        <div className='content'>
+          <Slider {...settings}>
+            {category.map((item) => (
+              <div className='boxs'>
+                <div className='box' key={item.id}>
+                  <img src={item.cover} alt='cover' />
+                  <div className='overlay'>
+                      <h4>{item.category}</h4>
+                    <p>{item.title}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </section>
+    </>
+  )
+}
