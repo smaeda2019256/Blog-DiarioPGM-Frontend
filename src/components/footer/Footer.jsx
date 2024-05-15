d14f26ef05486b76ea0618144e30ef7239ef90f5
Santiago Maeda <smaeda-2019256@Kinal.edu.gt>
2024-05-15 16:24:56 -0600

    Se sube el Footer para el Blog

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { AiFillTwitterCircle, AiFillLinkedin } from "react-icons/ai"
+import { BsFacebook } from "react-icons/bs"
+import { RiInstagramFill } from "react-icons/ri"
+
+export const Footer = () => {
+  return (
+    <>
+      <footer className='boxItems'>
+        <div className='container flex'>
+          <p>Santiago Maeda © - All right reserved - Blog El Diario del Programador</p>
+        </div>
+      </footer>
+    </>
+  )
+}
